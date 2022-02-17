38d2713fb9c27d991fca15fd28cd2c0c4a81adef
Vilas Patil <vilaspatil198@gmail.com>
2022-02-17 18:50:00 +0530

    deploy to heroku final

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """Flask function for cicd testing"""
     return "Hello World"
+
 
 if __name__ == "__main__":
     app.run()
